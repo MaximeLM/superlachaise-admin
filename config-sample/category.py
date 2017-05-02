66e96d111358b3f5bce9456ed111ed29b2a7bdd3
Maxime Le Moine <lm.maxime@gmail.com>
2017-05-02 13:08:25 +0200

    Export categories by kind

--- config-sample/category.py
+++ config-sample/category.py
@@ -16,8 +16,13 @@ CATEGORIES = [
 
 def get_category_export_object(config):
     categories = Category.objects.all()
+    categories_by_kind = {}
+    for category in categories:
+        if not category.kind in categories_by_kind:
+            categories_by_kind[category.kind] = []
+        categories_by_kind[category.kind].append(category)
     return {
-        "categories": {category.id: get_single_category_export_object(category, config.base.LANGUAGES) for category in categories},
+        "categories": {kind: {category.id: get_single_category_export_object(category, config.base.LANGUAGES) for category in categories} for (kind, categories) in categories_by_kind.items()},
     }
 
 def get_single_category_export_object(category, languages):
