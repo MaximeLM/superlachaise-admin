619bf9d21e76e19245ca9da3c388e88f2012e303
Maxime Le Moine <lm.maxime@gmail.com>
2017-11-25 16:47:35 +0100

    Create project

---
+++ SuperLachaiseAdmin/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SuperLachaiseAdmin
+//
+//  Created by Maxime Le Moine on 25/11/2017.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SuperLachaiseAdmin/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SuperLachaiseAdmin
+//
+//  Created by Maxime Le Moine on 25/11/2017.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
