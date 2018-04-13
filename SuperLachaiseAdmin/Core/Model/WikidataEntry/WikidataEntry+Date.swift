fae627c28b3fcee5d332ae4d0f8c0156872a8835
Maxime Le Moine <lm.maxime@gmail.com>
2018-04-13 21:06:00 +0200

    Rename EntryDate to WikidataEntryDate

--- SuperLachaiseAdmin/Core/Model/WikidataEntry/WikidataEntry+Date.swift
+++ SuperLachaiseAdmin/Core/Model/WikidataEntry/WikidataEntry+Date.swift
@@ -7,18 +7,67 @@
 
 import Foundation
 
+struct WikidataEntryDate {
+
+    enum Precision: String {
+        case day, month, year
+    }
+
+    let date: Date
+    let precision: Precision
+
+}
+
+extension WikidataEntryDate: CustomStringConvertible {
+
+    func dateString(template: String) -> String {
+        return dateFormatter(template: template).string(from: date)
+    }
+
+    private func dateFormatter(template: String) -> DateFormatter {
+        var template = template
+        switch precision {
+        case .year:
+            template = template.replacingOccurrences(of: "M", with: "")
+            template = template.replacingOccurrences(of: "d", with: "")
+        case .month:
+            template = template.replacingOccurrences(of: "d", with: "")
+        case .day:
+            break
+        }
+
+        let threadDict = Thread.current.threadDictionary
+        let key = "SuperLachaise.WikidataEntryDate.dateFormatters.\(template)"
+        if let dateFormatter = threadDict[key] as? DateFormatter {
+            return dateFormatter
+        } else {
+            let dateFormatter = DateFormatter()
+            dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+            dateFormatter.locale = Locale.current
+            dateFormatter.setLocalizedDateFormatFromTemplate(template)
+            threadDict[key] = dateFormatter
+            return dateFormatter
+        }
+    }
+
+    var description: String {
+        return dateString(template: "dMMMMYYYY")
+    }
+
+}
+
 extension WikidataEntry {
 
-    var dateOfBirth: EntryDate? {
+    var dateOfBirth: WikidataEntryDate? {
         get {
             guard let date = rawDateOfBirth else {
                 return nil
             }
-            guard let precision = EntryDate.Precision(rawValue: rawDateOfBirthPrecision) else {
+            guard let precision = WikidataEntryDate.Precision(rawValue: rawDateOfBirthPrecision) else {
                 Logger.warning("Invalid rawDateOfBirthPrecision: \(rawDateOfBirthPrecision)")
                 return nil
             }
-            return EntryDate(date: date, precision: precision)
+            return WikidataEntryDate(date: date, precision: precision)
         }
         set {
             rawDateOfBirth = newValue?.date
@@ -26,16 +75,16 @@ extension WikidataEntry {
         }
     }
 
-    var dateOfDeath: EntryDate? {
+    var dateOfDeath: WikidataEntryDate? {
         get {
             guard let date = rawDateOfDeath else {
                 return nil
             }
-            guard let precision = EntryDate.Precision(rawValue: rawDateOfDeathPrecision) else {
+            guard let precision = WikidataEntryDate.Precision(rawValue: rawDateOfDeathPrecision) else {
                 Logger.warning("Invalid rawDateOfDeathPrecision: \(rawDateOfDeathPrecision)")
                 return nil
             }
-            return EntryDate(date: date, precision: precision)
+            return WikidataEntryDate(date: date, precision: precision)
         }
         set {
             rawDateOfDeath = newValue?.date
