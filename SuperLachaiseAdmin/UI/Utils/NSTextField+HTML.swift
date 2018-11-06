d9fc82da6253eb2b296b81a406900693d18a2863
Maxime Le Moine <lm.maxime@gmail.com>
2018-11-06 23:00:21 +0100

    UI fixes

--- SuperLachaiseAdmin/UI/Utils/NSTextField+HTML.swift
+++ SuperLachaiseAdmin/UI/Utils/NSTextField+HTML.swift
@@ -15,8 +15,15 @@ extension NSTextField {
         }
         set {
             let font = self.font ?? NSFont.systemFont(ofSize: NSFont.systemFontSize)
+            let color = (self.textColor ?? .labelColor).usingColorSpaceName(.deviceRGB)
+                ?? NSColor(calibratedRed: 0, green: 0, blue: 0, alpha: 1)
+            var red: CGFloat = 0
+            var green: CGFloat = 0
+            var blue: CGFloat = 0
+            color.getRed(&red, green: &green, blue: &blue, alpha: nil)
+            let cssColor = String(format: "#%02x%02x%02x", Int(red * 255.0), Int(green * 255.0), Int(blue * 255.0))
             let style = """
-            font-family:'\(font.fontName)'; font-size:\(font.pointSize)px;
+            font-family:'\(font.fontName)'; font-size:\(font.pointSize)px; color:\(cssColor);
             """
             let html = "<span style=\"\(style)\">\(newValue)</span>"
             guard let data = html.data(using: .utf8) else {
